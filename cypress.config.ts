3de6ecc6fac5f70ad53a9738faaad0e2a62d6625
Filip Hric <filip@filiphric.com>
2023-07-10 16:15:12 +0200

    update packages and fix cypress test errors

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,13 +3,11 @@ import { defineConfig } from "cypress";
 import { resetDbTask } from './cypress/tasks/resetDb';
 import replay from "@replayio/cypress";
 
-
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('task', { resetDbTask })
-      // @ts-ignore figure out later
-      replay.default(on, config);
+      replay(on, config);
 
       return config
     },
